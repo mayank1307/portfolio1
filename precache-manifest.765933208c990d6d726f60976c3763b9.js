0385abdebf4f1a953b6e2f248cdbe085743cdab1
mayank1307 <mayankku1307@gmail.com>
2020-05-22 02:32:22 +0530

    Updates

---
+++ precache-manifest.765933208c990d6d726f60976c3763b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05de69c49731c659e3c5edb0323f7599",
+    "url": "/port/index.html"
+  },
+  {
+    "revision": "15554a88d4f19086a6a1",
+    "url": "/port/static/css/main.62e072d8.chunk.css"
+  },
+  {
+    "revision": "9d0921e78c4bc9f04e31",
+    "url": "/port/static/js/2.b624b625.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/port/static/js/2.b624b625.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15554a88d4f19086a6a1",
+    "url": "/port/static/js/main.68d37e73.chunk.js"
+  },
+  {
+    "revision": "8c66866c1c40b42b5ecf",
+    "url": "/port/static/js/runtime-main.7681dbd0.js"
+  }
+]);
